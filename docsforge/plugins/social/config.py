8be58fdae18f408effc9b4e0555483f59428f1ad
QQ <qq@example.com>
2026-05-10 12:08:49 +0800

    docsforge: Change license from MIT to LGPL v3
    - Replaced LICENSE file with GNU Lesser General Public License v3
    - Updated pyproject.toml: GPL->LGPL, classifier updated
    - Replaced all MIT copyright headers in vendored Python files with LGPL v3
    - Third-party JS libraries (lunr, html5shiv) retain original licenses

--- docsforge/plugins/social/config.py
+++ docsforge/plugins/social/config.py
@@ -1,11 +1,22 @@
 # Copyright (c) 2016-2025 Martin Donath <martin.donath@squidfunk.com>
+# Copyright (c) 2026 QQ (Cyrus)
+#
+# This file is part of DocsForge.
+#
+# DocsForge is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# DocsForge is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with DocsForge.  If not, see <https://www.gnu.org/licenses/>.
 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to
-# deal in the Software without restriction, including without limitation the
-# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-# sell copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
+
 
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
