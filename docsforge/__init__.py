8058e8da2d256273df972db25bbc48e0ef84234e
QQ <qq@example.com>
2026-05-19 19:16:07 +0800

    chore: bump version to 10.4.1

--- docsforge/__init__.py
+++ docsforge/__init__.py
@@ -4,7 +4,7 @@ DocsForge combines the DocsForge build engine with the Material for MkDocs
 theme and plugins into a single, cohesive package.
 """
 
-__version__ = "10.4.0"
+__version__ = "10.4.1"
 __prog_name__ = "docsforge"
 
 # Install vendor compatibility shims
